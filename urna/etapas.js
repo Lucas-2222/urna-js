c4535551c3153cd9fe1205719081d636a5bf9356
Lucas Mello <lucas@salato.com.br>
2023-01-10 22:16:11 -0300

    Réplica de Urna eletrônica

---
+++ urna/etapas.js
@@ -0,0 +1,51 @@
+let etapas = [
+    {
+        titulo: 'DEPUTADO',
+        numeros: 5,
+        candidatos: [
+            
+            {
+                numero: '14123',
+                nome: 'Felipe da Gráfica',
+                partido: 'PL',
+                fotos:[
+                    {url:'felipe.png', legenda: 'DEPUTADO', small: true}
+                ]
+            },
+            {
+                numero: '23222',
+                nome: 'Robertin do gelo',
+                partido: 'PT',
+                fotos:[
+                    {url:'robertin.png', legenda: 'DEPUTADO', }
+                ]
+            },
+            ]
+    },
+    {
+        titulo: 'PRESIDENTE',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '13',
+                nome: 'Lula',
+                partido: 'PT',
+                vice: 'Geraldo',
+                fotos:[
+                    {url:'lula2.png', legenda: 'Presidente', },
+                    {url:'alckmi.png', legenda: 'Vice-Presidente', small: true }
+                ]
+            },
+            {
+                numero: '22',
+                nome: 'Bolsonaro',
+                partido: 'PL',
+                vice: 'Braga Neto',
+                fotos:[
+                    {url:'bolsonaro.png', legenda: 'Presidente', },
+                    {url:'braganeto.png', legenda: 'Vice-Presidente', small: true }
+                ]
+            },
+        ]
+    }
+];
